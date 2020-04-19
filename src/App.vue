f7c01d2be30adb7581271ffd2d95e92d56ed8b65
Gints Polis <gints.polis@balcia.com>
2020-04-19 20:52:10 +0300

    Bit nicer menu

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/products">Products</router-link>|
-      <router-link to="/admin">Admin</router-link>|
-      <router-link to="/about">About</router-link>|
-      <router-link v-show="!$store.state.user" to="/login">Login</router-link>
-      <router-link v-show="!!$store.state.user" to="/logout">Logout</router-link>
-    </div>
     <v-app>
+      <v-app-bar app color="secondary" dark>
+        <v-tabs align-with-title>
+          <v-tab to="/">Home</v-tab>
+          <v-tab to="/products">Products</v-tab>
+          <v-tab to="/admin">Admin</v-tab>
+          <v-tab to="/about">About</v-tab>
+        </v-tabs>
+        <v-spacer></v-spacer>
+        <v-btn to="/login" v-show="!$store.state.user" text>Login</v-btn>
+        <v-btn to="/logout" v-show="!!$store.state.user" text>Logout</v-btn>
+      </v-app-bar>
       <v-content>
         <v-container fluid>
           <v-row>
@@ -23,26 +26,3 @@
     </v-app>
   </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
