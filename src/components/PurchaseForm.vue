1ddc2d978e7a703ae351c1f1020bd8317f04e887
Gints Polis <gints.polis@balcia.com>
2020-04-28 20:23:53 +0300

    Slide transaction on product appear.

--- src/components/PurchaseForm.vue
+++ src/components/PurchaseForm.vue
@@ -1,31 +1,35 @@
 <template>
   <div>
-    <v-row
-      align="center"
-      justify="center"
-      style="height: 300px;"
-      v-if="(!($store.state.user.user && $store.state.user.policy.policies) || !($store.state.classifier.productTypes[1]))"
-    >
-      <v-progress-circular indeterminate color="primary"></v-progress-circular>
-    </v-row>
-    <div v-if="$store.state.user.user && $store.state.user.policy.policies">
-      Your active policies:
-      <br />
-      <div v-for="policy in $store.state.user.policy.policies" v-bind:key="policy.id">
-        {{policy.number}}
+    <v-slide-y-transition mode="out-in">
+      <div v-if="$store.state.user.user && $store.state.user.policy.policies">
+        Your active policies:
         <br />
+        <div v-for="policy in $store.state.user.policy.policies" v-bind:key="policy.id">
+          {{policy.number}}
+          <br />
+        </div>
       </div>
-    </div>
-    <v-row>
-      <v-col
-        v-for="type in $store.state.classifier.productTypes"
-        v-bind:key="type.id"
-        lg="4"
-        md="6"
+    </v-slide-y-transition>
+    <v-slide-y-transition mode="out-in">
+      <v-row v-if="$store.state.classifier.productTypes[0]">
+        <v-col
+          v-for="type in $store.state.classifier.productTypes"
+          v-bind:key="type.id"
+          lg="4"
+          md="6"
+        >
+          <product-card :type="type" />
+        </v-col>
+      </v-row>
+      <v-row
+        align="center"
+        justify="center"
+        style="height: 300px;"
+        v-if="(!($store.state.user.user && $store.state.user.policy.policies) || !($store.state.classifier.productTypes[1]))"
       >
-        <product-card :type="type" />
-      </v-col>
-    </v-row>
+        <v-progress-circular indeterminate color="primary"></v-progress-circular>
+      </v-row>
+    </v-slide-y-transition>
   </div>
 </template>
 
